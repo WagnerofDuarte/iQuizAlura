081d7eca2d1df271bec5602a4ab2671a8f34fe8a
Wagner Duarte <46401074+WagnerofDuarte@users.noreply.github.com>
2023-09-05 12:47:02 -0300

    Update Questao.swift
    final

--- iQuizAlura/Models/Questao.swift
+++ iQuizAlura/Models/Questao.swift
@@ -19,7 +19,7 @@ Questao(titulo: "Em que ano Vingadores Ultimato foi lançado?", respostas: ["202
 Questao(titulo: "Qual a sub-classe mais apelona no Destiny 2", respostas: ["Arcstrider", "Voidwalker", "Striker"], respostaCorreta: 2),
 Questao(titulo: "Porque Destiny 2 é uma decepcao?", respostas: ["Pq a Bungie só faz cagada", "O jogo tá caro p caramba", "Sou hater"], respostaCorreta: 0),
 Questao(titulo: "Porque Destiny 2 é uma delícia?", respostas: ["Pq a Bungie é perfeira", "Pq o jogo é gratis", "Sou lover"], respostaCorreta: 0),
-Questao(titulo: "Qual a primeira empresa que voce vai trabalhar", respostas: ["Google", "DTI Crafters", "PornHub"], respostaCorreta: 1),
+Questao(titulo: "Qual a empresa que voce trabalha?", respostas: ["Google", "DTI Crafters", "Itau"], respostaCorreta: 1),
 Questao(titulo: "Quantas vezes o Lula foi eleito? ", respostas: ["4", "3", "1"], respostaCorreta: 1),
 Questao(titulo: "O aquecimento global vai matar geral?", respostas: ["Com certeza", "Vai nada, isso é invenção da esque...", "É só fazer um super ar condicionado"], respostaCorreta: 0),
 Questao(titulo: "Porque a vida é tão difícil?", respostas: ["Pq sim", "Pq vc n é milionário", "..."], respostaCorreta: 1),
